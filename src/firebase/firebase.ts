2faffa32513d7f5041334b7a13a895c6b9a8ac0f
Mahfuzur Rahman <90880886+Moon422@users.noreply.github.com>
2023-08-30 16:59:16 +0600

    removed firebase config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,21 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDZM8SQMepKyBbB7Q-fJkF1Sxufrj9Yo4g",
-    authDomain: "portfolio-base-39f19.firebaseapp.com",
-    projectId: "portfolio-base-39f19",
-    storageBucket: "portfolio-base-39f19.appspot.com",
-    messagingSenderId: "828553722108",
-    appId: "1:828553722108:web:56198516e958e7a12e7f79",
-    measurementId: "G-KFD3JTFT5M"
+
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+const db = getFirestore()
